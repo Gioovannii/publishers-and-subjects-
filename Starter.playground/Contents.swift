ad25db262772568aff10bda23c9a568e3bea2a97
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-27 13:35:05 +0800

    Initial Commit

---
+++ Starter.playground/Contents.swift
@@ -0,0 +1,63 @@
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+
+import Foundation
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+//example(of: "Publisher") {
+//    // 1 Create notification name
+//    let myNotification = Notification.Name("MyNotification")
+//
+//    // 2 Access notification center call it's publisher method and assign value to a constant
+//    let publisher = NotificationCenter.default
+//        .publisher(for: myNotification, object: nil)
+//
+//    // 3 Get a handle to the notification center
+//    let center = NotificationCenter.default
+//
+//    // 4 Create observer to listen for notification
+//    let observer = center.addObserver(forName: myNotification, object: nil, queue: nil) { notification in
+//        print("Notification received!")
+//    }
+//    // 5 Post notification with that name
+//    center.post(name: myNotification, object: nil)
+//
+//    // 6 Remove observer from notification center
+//    center.removeObserver(observer)
+//}
+
+
+
+
