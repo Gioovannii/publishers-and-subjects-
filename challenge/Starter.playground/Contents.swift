f9bfdd09089762af1de280ffff95bc78da0691e2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-06 21:08:16 +0800

    indent

--- challenge/Starter.playground/Contents.swift
+++ challenge/Starter.playground/Contents.swift
@@ -4,18 +4,27 @@ import Combine
 var subscriptions = Set<AnyCancellable>()
 
 example(of: "Create a Blackjack card dealer") {
-  let dealtHand = PassthroughSubject<Hand, HandError>()
-  
-  func deal(_ cardCount: UInt) {
-    var deck = cards
-    var cardsRemaining = 52
-    var hand = Hand()
+    let dealtHand = PassthroughSubject<Hand, HandError>()
     
-    for _ in 0 ..< cardCount {
-      let randomIndex = Int.random(in: 0 ..< cardsRemaining)
-      hand.append(deck[randomIndex])
-      deck.remove(at: randomIndex)
-      cardsRemaining -= 1
+    
+    func deal(_ cardCount: UInt) {
+        var deck = cards
+        var cardsRemaining = 52
+        var hand = Hand()
+        
+        for _ in 0 ..< cardCount {
+            let randomIndex = Int.random(in: 0 ..< cardsRemaining)
+            hand.append(deck[randomIndex])
+            deck.remove(at: randomIndex)
+            cardsRemaining -= 1
+        }
+        
+        // Add code to update dealtHand here
+        if hand.points > 21 {
+            dealtHand.send(completion: .failure(.busted))
+        } else {
+            dealtHand.send(hand)
+        }
     }
     
     // Add code to update dealtHand here
